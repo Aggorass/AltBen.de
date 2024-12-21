1b3593c4c559d114c31feed4bbef4968637dfb82
Dani <not@happening.de>
2024-12-21 16:45:19 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
-// Header & Footer dynamisch laden
-window.addEventListener('DOMContentLoaded', () => {
-    // Header
-    fetch('includes/header.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('header-placeholder').innerHTML = data;
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('includes/header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header-placeholder').innerHTML = data;
+
+      const menuToggle = document.querySelector('.menu-toggle');
+      const navUl = document.querySelector('nav ul');
+
+      menuToggle.addEventListener('click', () => {
+        navUl.classList.toggle('active');
       });
-  
-    // Footer
-    fetch('includes/footer.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('footer-placeholder').innerHTML = data;
-      });
-  });
-  
+    });
+
+  fetch('includes/footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer-placeholder').innerHTML = data;
+    });
+});
